e0c0b4b433aa38e8fa051640e31d47c4e24b0579
kaevin1004 <kaevin1004@naver.com>
2017-09-21 20:29:27 +0900

    1242

--- JAVA/Work/javaexec/src/main/java/tw04/tw04_05_합계구하기swap.java
+++ JAVA/Work/javaexec/src/main/java/tw04/tw04_05_합계구하기swap.java
@@ -5,30 +5,33 @@ import java.util.Scanner;
 public class tw04_05_합계구하기swap {
     public static void main(String[] args){
         
-        int s=0;
+        int p = 0;
         
         Scanner keyboard = new Scanner(System.in);
         System.out.print("시작 값:");
-        s=keyboard.nextInt();
+        p=keyboard.nextInt();
         
-        int f=0;
+        int s = 0;
         System.out.print("종료 값:");
-        f=keyboard.nextInt();
-        
-        int temp;//종료값을 temp에 넣는다.
+        s = keyboard.nextInt();
+      
+        if(p>s){
+          int temp=s;//종료값을 temp에 넣는다
+            s=p;//시작 값을 종료 값에 넣는다
+            p=temp;//temp를 시작값에 넣는다
+            System.out.println(p);
+            System.out.println(s);
+            System.out.println(temp);
+        }
+        else {
+            
+        }
         int sum=0;
         
-        if(s>f){
-                temp=s;
-                s=f;
-                f=temp;
+        for(int i =p; i<=s; i=i+1){
+            sum = sum+i;           
         }
-        
-    for(int i =s; i<=f; i=i+1){
-        sum = sum+i;
-    }
-    System.out.println("합계"+sum);
-            
+        System.out.println(sum);
     }
     
 }
